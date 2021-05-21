2cbfe7584a12ef8b4bb7661236f4b9fd3afb5ff7
Brandon Iles <brandon@ampleforth.org>
2021-05-21 10:50:45 -0700

    fix lint

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -10,17 +10,17 @@ import { DELAY } from './utils'
 chai.use(solidity)
 
 interface GovernanceFixture {
-    forth: Contract
+  forth: Contract
 }
 
 export async function governanceFixture(
-    [wallet]: Wallet[],
-    provider: providers.Web3Provider
+  [wallet]: Wallet[],
+  provider: providers.Web3Provider
 ): Promise<GovernanceFixture> {
-    // deploy FORTH, sending the total supply to the deployer
-    const { timestamp: now } = await provider.getBlock('latest')
-    const timelockAddress = Contract.getContractAddress({ from: wallet.address, nonce: 1 })
-    const forth = await deployContract(wallet, Forth, [wallet.address, timelockAddress, now + 60 * 60])
+  // deploy FORTH, sending the total supply to the deployer
+  const { timestamp: now } = await provider.getBlock('latest')
+  const timelockAddress = Contract.getContractAddress({ from: wallet.address, nonce: 1 })
+  const forth = await deployContract(wallet, Forth, [wallet.address, timelockAddress, now + 60 * 60])
 
-    return { forth }
+  return { forth }
 }
